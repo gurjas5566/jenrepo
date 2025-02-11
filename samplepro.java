16420e9d13d3ebdb6a993cb9428d767f8e896426
gurjas5566 <gurjassingh5046@gmail.com>
2025-02-11 15:13:26 +0530

    Update samplepro.java

--- samplepro.java
+++ samplepro.java
@@ -1,7 +1,7 @@
 public class samplepro
 {
- public static void main(String[] args)
-{
-  System.out.println("Hello Jenkins");
-}
+  public static void main(String[] args)
+    {
+        System.out.println("My First Java Program.");
+    }
 }
